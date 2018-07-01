3f2ef86b29bde3875a1dd8f49dc2363fb69c71d8
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2018-07-01 15:34:03 -0400

    V1.12.6 | Tempname Patch 04
    ~Changed naming statement...again
    ~Removed bool for inTempCh and replaced it with the check function

--- Commands/Active/tempname.js
+++ Commands/Active/tempname.js
@@ -5,15 +5,14 @@ const chs = require('../../Modules/fs_channels.json');
 exports.run = (fishsticks, msg, cmd) => {
     msg.delete();
 
-    var newName = cmd[0] ? cmd.slice(1).join(" ") : cmd.join(' ');
+    var newName = cmd.join(' ');
     var userChannel = msg.member.voiceChannel;
-    let inTempCh = false;
 
     var ranger = fishsticks.users.get("107203929447616512");
 
     for (c = 0; c < fishsticks.tempChannels.length; c++) {
         if (userChannel == (fishsticks.channels.get(fishsticks.tempChannels[c]))) {
-            inTempCh == true;
+            check();
         }
     }
 
@@ -25,60 +24,62 @@ exports.run = (fishsticks, msg, cmd) => {
         msg.reply("Channel name changed to " + newName + " successfully!").then(sent => sent.delete(10000));
     }
 
-    if (fishsticks.engmode == true) { //CHECK ENGINEERING MODE
-        console.log("[TEMP-CHA] Channel name change attempted with ENGM enabled!");
-
-        if (msg.member.roles.find('name', 'Staff')) { //CHECK STAFF OVERRIDE DURING ENGINEERING MODE
-            console.log("[TEMP-CHA] Staff Override Received. Checking channel validity...");
+    function check() {
+        if (fishsticks.engmode == true) { //CHECK ENGINEERING MODE
+            console.log("[TEMP-CHA] Channel name change attempted with ENGM enabled!");
     
-            if (inTempCh == false) { //IF NOT IN TEMP CHANNEL, REJECT WITH NOTE
-                console.log("[TEMP-CHA] Not a temp channel, request ignored.");
-                msg.reply("Umm, this is not a temporary voice channel! Consult " + ranger + " before changing this channel's name!").then(sent => sent.delete(10000));
+            if (msg.member.roles.find('name', 'Staff')) { //CHECK STAFF OVERRIDE DURING ENGINEERING MODE
+                console.log("[TEMP-CHA] Staff Override Received. Checking channel validity...");
+        
+                if (inTempCh == false) { //IF NOT IN TEMP CHANNEL, REJECT WITH NOTE
+                    console.log("[TEMP-CHA] Not a temp channel, request ignored.");
+                    msg.reply("Umm, this is not a temporary voice channel! Consult " + ranger + " before changing this channel's name!").then(sent => sent.delete(10000));
+                }
+                else if (inTempCh == true) { //IF IN TEMP CHANNEL, ACCEPT
+                    msg.reply("Staff override recognized, changing channel name...").then(sent => sent.delete(10000));
+                    accept();
+                }
+                else { //IF UNKNOWN WTH IS GOING ON, REJECT AND CALL AID
+                    console.log("[TEMP-CHA] Something's wrong...check for errors?");
+                    msg.reply("Huh. Seems that something's not right. Let me call for assistance: " + ranger).then(sent => sent.delete(60000));
+                }
             }
-            else if (inTempCh == true) { //IF IN TEMP CHANNEL, ACCEPT
-                msg.reply("Staff override recognized, changing channel name...").then(sent => sent.delete(10000));
-                accept();
-            }
-            else { //IF UNKNOWN WTH IS GOING ON, REJECT AND CALL AID
-                console.log("[TEMP-CHA] Something's wrong...check for errors?");
-                msg.reply("Huh. Seems that something's not right. Let me call for assistance: " + ranger).then(sent => sent.delete(60000));
+            else {
+                console.log("[TEMP-CHA] Non-staff attempt. Ignoring request.");
+                msg.reply("Engineering Mode is enabled! Have someone disable it first!").then(sent => sent.delete(10000));
             }
         }
         else {
-            console.log("[TEMP-CHA] Non-staff attempt. Ignoring request.");
-            msg.reply("Engineering Mode is enabled! Have someone disable it first!").then(sent => sent.delete(10000));
-        }
-    }
-    else {
-        if (msg.member.roles.find('name', 'Staff')) {
-            console.log("[TEMP-CHA] Staff Override Received. Checking channel validity...")
-
-            if (inTempCh == false) { //IF NOT IN TEMP CHANNEL, REJECT WITH NOTE
-                console.log("[TEMP-CHA] Not a temp channel, request ignored.")
-                msg.reply("Umm, this is not a temporary voice channel! Consult " + ranger + " before changing this channel's name!").then(sent => sent.delete(10000));
+            if (msg.member.roles.find('name', 'Staff')) {
+                console.log("[TEMP-CHA] Staff Override Received. Checking channel validity...")
+    
+                if (inTempCh == false) { //IF NOT IN TEMP CHANNEL, REJECT WITH NOTE
+                    console.log("[TEMP-CHA] Not a temp channel, request ignored.")
+                    msg.reply("Umm, this is not a temporary voice channel! Consult " + ranger + " before changing this channel's name!").then(sent => sent.delete(10000));
+                }
+                else if (inTempCh == true) { //IF IN TEMP CHANNEL, ACCEPT
+                    msg.reply("Staff override recognized, changing channel name...").then(sent => sent.delete(10000));
+                    accept();
+                }
+                else { //IF UNKNOWN WTH IS GOING ON, REJECT AND CALL AID
+                    console.log("[TEMP-CHA] Something's wrong...check for errors?");
+                    msg.reply("Huh. Seems that something's not right. Let me call for assistance: " + ranger).then(sent => sent.delete(60000));
+                }
             }
-            else if (inTempCh == true) { //IF IN TEMP CHANNEL, ACCEPT
-                msg.reply("Staff override recognized, changing channel name...").then(sent => sent.delete(10000));
-                accept();
-            }
-            else { //IF UNKNOWN WTH IS GOING ON, REJECT AND CALL AID
-                console.log("[TEMP-CHA] Something's wrong...check for errors?");
-                msg.reply("Huh. Seems that something's not right. Let me call for assistance: " + ranger).then(sent => sent.delete(60000));
-            }
-        }
-        else if ((msg.member.roles.find('name', 'CC Member')) || (msg.member.roles.find('name', 'ACC Member'))) {
-            console.log("[TEMP-CHA] Non-staff command recieved. Checking channel validity...")
-            if (inTempCh == false) {
-                console.log("[TEMP-CHA] Not a temp channel, request ignored.")
-                msg.reply("You're not in a temporary voice channel! You can't change this channel's name!").then(sent => sent.delete(10000));
-            }
-            else if (inTempCh == true) {
-                msg.reply("Acknowledged. Attempting channel name change...");
-                accept();
-            }
-            else {
-                console.log("[TEMP-CHA] Something's wrong...check for errors?");
-                msg.reply("Huh. Seems that something's not right. Let me call for assistance: " + ranger).then(sent => sent.delete(60000));
+            else if ((msg.member.roles.find('name', 'CC Member')) || (msg.member.roles.find('name', 'ACC Member'))) {
+                console.log("[TEMP-CHA] Non-staff command recieved. Checking channel validity...")
+                if (inTempCh == false) {
+                    console.log("[TEMP-CHA] Not a temp channel, request ignored.")
+                    msg.reply("You're not in a temporary voice channel! You can't change this channel's name!").then(sent => sent.delete(10000));
+                }
+                else if (inTempCh == true) {
+                    msg.reply("Acknowledged. Attempting channel name change...");
+                    accept();
+                }
+                else {
+                    console.log("[TEMP-CHA] Something's wrong...check for errors?");
+                    msg.reply("Huh. Seems that something's not right. Let me call for assistance: " + ranger).then(sent => sent.delete(60000));
+                }
             }
         }
     }
