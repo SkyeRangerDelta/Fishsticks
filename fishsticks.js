984736f78b58aadcc8030bb36678aa07c14b71ed
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2018-09-25 17:41:53 -0400

    V1.14.27a | Sub-Patch 02
    ~Changed formatting.

--- fishsticks.js
+++ fishsticks.js
@@ -186,7 +186,7 @@ fishsticks.on('message', async msg => {
 	}
 
 	if (msg.content.toLowerCase() == "hello there") {
-		msg.reply("General Kenobi", {files: ["./images/grievous.gif"]});
+		msg.channel.send("General " + msg.author.tag, {files: ["./images/grievous.gif"]});
 		fishsticks.commandSuccess++;
 		return;
 	}
