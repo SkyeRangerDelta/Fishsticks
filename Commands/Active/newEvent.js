8b6c77a4c2ef0420c3dbf2e3dfaa3e50b3e847ec
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2019-11-21 18:37:17 -0500

    Core Script 1
    + Added pseudo-command newEvent that is a refactored event command. Foundation for Google API interation included.

---
+++ Commands/Active/newEvent.js
@@ -0,0 +1,237 @@
+//----NEW EVENT----
+
+//Dependencies 1
+const sys = require('../../Modules/fs_systems.json');
+const gapi = require('../../Modules/GoogleCalAPI.json');
+const perms = require('../../Modules/Functions/permissionsCheck.js');
+const syslog = require('../../Modules/Functions/syslog.js');
+const Discord = require('discord.js');
+
+const https = require('https');
+//const google = require('googleapis');
+
+/*
+const googleAuth = new google.auth.OAuth2(
+    gapi.client_id,
+    gapi.client_secret,
+    gapi.redirect_uris
+)
+*/
+
+//google.options({auth: googleAuth});
+
+exports.run = (fishsticks, msg, cmd) => {
+    msg.delete()
+
+    //Permissions Functions
+    let pms = {
+        "perms": ["Staff", "Bot", "Event Coordinator"]
+    }
+    let verification = perms.run(fishsticks, msg.member, pms);
+
+    if (!verification) {
+        return msg.reply("You are not authorized to run this command!");
+    }
+
+    //DEPENDENCIES 2
+    //Setup log method
+    function log(msg, level) {
+        syslog.run(fishsticks, "[EVENT-SYS] " + msg, level)
+    }
+
+    //Deploy log message that the command is now executing
+    log("Beginning event construction and deployment.", 2);
+
+    //Event Object
+    let event = {
+        name: "",
+        host: "",
+        day: 1,
+        month: 1,
+        year: 2000,
+        time_HR: 12,
+        time_HR24: 12,
+        time_M: 0,
+        meridiem: "",
+        desc: "",
+        url_date: "",
+        url_endDate: ""
+    }
+
+    //Command Breakup
+    let cmdAlt = msg.content.split('-'); //Set - as delimiter
+    log("Parameters Collected: " + cmdAlt, 2); //Log the parameters
+
+    event.host = msg.author.username; //Set message euther has host
+
+    //CHECK FUNCTIONS
+    //Deploy log message to notify that checks are in progress
+    log("Verifying command integrity...", 2);
+
+    //Verify name field not invalid
+    if (cmdAlt[1] == null || cmdAlt[1] == undefined || typeof cmdAlt[1] != typeof "blah") {
+        return msg.reply("I don't have a name for the event! How can you host an event with no name?")
+        .then(sent => sent.delete(10000)); //If no parameter, return with error
+    } else {
+        event.name = cmdAlt[1]; //Else, set name
+    }
+
+    //UTILITY LOGIC
+    //Generate the date
+    let currDate = new Date();
+
+    //Get date from command
+    let time = cmdAlt[2].trim(); //Remove whitespace
+    time = time.split(":"); //Set : as delimiter
+
+    //Date format: MM:DD:YYYY:HrHr:MiMi:Meridiem
+    //Verify that time even exists
+    if ((cmdAlt[2] == undefined || cmdAlt[2] == null) && typeof cmdAlt[2] == typeof "blah") {
+        return msg.reply("Events don't get planned like this without a date. Hit me with something here.")
+        .then(sent => sent.delete(10000)); //If not, return error response
+    }
+
+    //Calculate month
+    //Verify valid
+    if ((time[0] > 0 && time[0] < 13) && time[1] != undefined) {
+        event.month = parseInt(time[0]);
+    } else {
+        return msg.reply("You know there are only 12 months right? (Has to be a unmber too.)")
+        .then(sent => sent.delete(10000));
+    }
+
+    //Calculate day
+    //NOTE: This system assumes that with the exception of Feb. only, each month has 32 days.
+    if (event.month != 2) {
+        if ((time[1] > 0 && time[1] < 33) && time[1] != undefined) {
+            event.day = parseInt(time[1]);
+        } else {
+            return msg.reply("That's not a day in this month! C'mon.")
+            .then(sent => sent.delete(10000));
+        }
+    } else if (event.month == 2) {
+        if ((time[1] > 0 && time[1] < 29) && time[1] != undefined) {
+            event.day = parseInt(time[1]);
+        } else {
+            return msg.reply("February only has 28 days (and in my system there are no leap years).")
+            .then(sent => sent.delete(10000));
+        }
+    } else {
+        return msg.reply("Something...wait, how did you get here. Try that one again.")
+        .then(sent => sent.delete(10000));
+    }
+
+    //Calculate year
+    if ((time[2] < currDate.getFullYear()) && time[2] != undefined) {
+        return msg.reply("We live in " + currDate.getFullYear() + ". Can't schedule before that.")
+        .then(sent => sent.delete(10000));
+    } else if (time[2] > 9999) {
+        return msg.reply("Wow, trying to schedule a game night for your 18x grandkids? No, go get another bot for that.")
+        .then(sent => sent.delete(10000));
+    } else {
+        event.year = parseInt(time[2]);
+    }
+
+    //Calculate hour
+    if ((time[3] > 0 && time[3] < 13) && time[3] != undefined) {
+        event.time_HR = parseInt(time[3]);
+    } else if (time[3] > 12 && time[3] < 24 && time[3] != undefined) {
+        return msg.reply("Hey hey, I don't do any of that tWeNtY-fOuR hOuR mess. I want actual hours.")
+        .then(sent => sent.delete(10000));
+    } else {
+        return msg.reply("You...have gone horribly wrong somewhere. We're not working with relativity here, just a standard clock.")
+        .then(sent => sent.delete(10000));
+    }
+
+    //Calculate minute
+    if (time[4] >= 0 && time[4] <= 59 && (time[4] != undefined)) {
+        event.time_M = parseInt(time[4]);
+    } else {
+        return msg.reply("Mmmmmmmmmmmmmmmmmmmmmmmmmminutes...only run from 0 to 59.")
+        .then(sent => sent.delete(10000));
+    }
+
+    //Calulate meridiem
+    if (time[5].toLowerCase() == "am" | time[5].toLowerCase() == "pm") {
+        event.meridiem = time[5];
+    } else {
+        return msg.reply("Remember. 12. Hours. I'm gonna need either AM or PM.")
+        .then(sent => sent.delete(10000));
+    }
+
+    if (event.meridiem.toLowerCase() == "pm") {
+        event.time_HR24 = event.time_HR + 12;
+    }
+
+    //Build date for verification
+    let month = currDate.getMonth()+1;
+    let date = currDate.getDay()+1;
+    let hr = currDate.getHours();
+    let min = currDate.getMinutes();
+
+    if (event.month < month) {
+        return msg.reply("[Month] No dates in the past!")
+        .then(sent => sent.delete(10000))
+    } else if (event.day < date) {
+        return msg.reply("[Day] No dates in the past!")
+        .then(sent => sent.delete(10000))
+    } else if (event.time_HR24 < hr) {
+        return msg.reply("[Hour] No dates in the past!")
+        .then(sent => sent.delete(10000))
+    } else if (event.time_M < min) {
+        return msg.reply("[Minute] No dates in the past!")
+        .then(sent => sent.delete(10000))
+    }
+
+    //Build payload date
+    event.url_date = `${event.month}/${event.day}/${event.year} ${event.time_HR}:${event.time_M}${event.meridiem}`;
+    event.url_endDate = `${event.month}/${event.day}/${event.year} ${event.time_HR+1}:${event.time_M}${event.meridiem}`;
+
+    //Build description
+    event.desc = cmdAlt[3].trim();
+
+    //Verify event using Embed
+    let eventPanel = new Discord.RichEmbed();
+        eventPanel.setTitle("New Event: " + event.name);
+        eventPanel.setDescription(event.desc);
+        eventPanel.addField("Save the Date!", `${event.month}/${event.day}/${event.year} at ${event.time_HR}:${event.time_M}${event.meridiem}`);
+        eventPanel.setFooter("Coordinated by " + event.host + ". Automated by yours truely.");
+    
+    msg.channel.send({embed: eventPanel});
+
+    /*
+    //HTTPS SENDS
+    async function processAuth(authScopes) {
+        return new Promise((resolve, reject) => {
+            const authURL = googleAuth.generateAuthUrl({
+                access_type: 'offline',
+                scope: authScopes.join(' ')
+            });
+
+            const server = https.createServer(async (req, res) => {
+                try {
+                    if (req.url.indexOf('/oauth2callback') > -1) {
+                        const qs = new url.URL(req.url, 'http://localhost:3000')
+                            .searchParams;
+                        res.end('Auth successfull!');
+                        log("Auth validated.", 3);
+                        server.destroy();
+
+                        const {tokens} = await googleAuth.getToken(qs.get('code'));
+                        googleAuth.credentials = tokens;
+                        resolve(googleAuth);
+                    }
+                } catch (authReqErr) {
+                    reject(authReqErr);
+                }
+            });
+        });
+    }
+
+    async function runEvent() {
+        const res = await calendar.events.insert
+    }
+
+    */
+    
+}
