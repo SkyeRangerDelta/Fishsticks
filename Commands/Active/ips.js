e880cc27a8f872b00905a0286549e57d639429e8
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2020-06-03 23:05:52 -0400

    V1.17.7 Patch 2
    I fixed the IP thing. Whatever.

--- Commands/Active/ips.js
+++ Commands/Active/ips.js
@@ -2,14 +2,17 @@ const Discord = require('discord.js');
 const config = require('../../Modules/Core/corecfg.json');
 
 exports.run = (fishsticks, msg, cmd) => {
-    msg.delete();
+    msg.delete({timeout: 0});
 
-    var ips = new Discord.RichEmbed();
+    var ips = new Discord.MessageEmbed();
 		ips.setTitle("o0o - CC 'THE FISH' SERVERS - o0o")
 		ips.setColor(config.fscolor)
 		ips.setDescription(
-			"CC currently doesn't host any game servers.\n\nPersnaps you could send SkyeRangerDelta a message?"
+			`CCG recognized servers and their IPS.`
 		);
+		ips.addField(`CCG Official Servers`, "ARK: Survival Evolved (Admin: Nils Sargon): `192.99.126.106:41213`\n" +
+		"Terraria (Admin: Winged Scribe): `pldyn.net:41213`\n" +
+		"Minecraft (Admin: FutronBob): `Pending Addition`");
 
-    msg.channel.send({embed: ips}).then(sent => sent.delete(30000));
+    msg.channel.send({embed: ips}).then(sent => sent.delete({timeout: 30000}));
 }
